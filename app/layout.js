9eb36c61e7819de5bc9a80d80084d72756d931fb
jim.chen <azx011014@gmail.com>
2024-11-16 20:25:25 +0800

    feat: enhance layout and page component visuals
    - Change `body` class to include `relative` in `layout.js`
    - Add `Link` and `Image` imports to `page.js`
    - Replace introductory text with an enhanced visual and link component in `page.js`
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased min-h-screen flex flex-col bg-primary-950 text-primary-100`}
+        className={`${josefin.className} relative antialiased min-h-screen flex flex-col bg-primary-950 text-primary-100`}
       >
         <Header />
 
